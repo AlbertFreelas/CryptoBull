177e152f6f41e8627dd6bf83cdfd4922d38d9388
AlbertSamuelMelo <asm.asm.albert@icloud.com>
2021-12-29 08:59:32 -0400

    second block
    Signed-off-by: AlbertSamuelMelo <asm.asm.albert@icloud.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import FirstBlock from './components/pages/FristBlock/FirstBlock';
+import SecondBlock from './components/pages/SecondBlock/SecondBlock';
 
 function App() {
   return (
     <div className="App">
       <body>
       <FirstBlock/>
+      <SecondBlock/>
       </body>
     </div>
   );
---
+++ src/components/pages/SecondBlock/SecondBlock.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import wellcome from './../../../second-block/WELCOME-BANNER@2x.png'
+import text from './../../../second-block/TEXTO01-BLC02@2x.png'
+import box from './../../../second-block/IMG01-BLC02@2x.png' 
+import santaBull from './../../../second-block/IMG02-BLC02@2x.png' 
+
+import { useState, useEffect } from 'react';
+
+function SecondBlock() {
+    const [width, setWidth] = useState(window.innerWidth)
+    const [SantaBullDivHeight, setSantaBullDivHeight] = useState(0)
+
+    useEffect(() => {
+        window.addEventListener('load', () => {
+            var updatedHeight = document.getElementById("santaBullDiv").clientHeight
+            console.log(updatedHeight)
+            setSantaBullDivHeight(updatedHeight)
+        });
+        window.addEventListener('resize', () => {
+            setWidth(window.innerWidth)
+            var updatedHeight = document.getElementById("santaBullDiv").clientHeight
+            console.log(updatedHeight)
+            setSantaBullDivHeight(updatedHeight)
+        });
+      });
+
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            flex: "auto",
+        }}>
+            <div>
+                <img 
+                    src={wellcome} 
+                    style={{
+                        width: width * 0.6,
+                        marginLeft: "10%",
+                        marginRight: "10%",
+                        marginTop: "3%"
+                    }} 
+                    alt="BullsCryptoWellCome" 
+                />
+            </div>
+            <div>
+                <img 
+                    src={text} 
+                    style={{
+                        width: width * 0.56,
+                        marginLeft: "12%",
+                        marginRight: "12%",
+                        marginTop: "2%"
+                    }} 
+                    alt="BullsCryptoText" 
+                />
+            </div>
+            <div>
+                <img 
+                    src={box} 
+                    style={{
+                        width: width * 0.6,
+                        marginLeft: "10%",
+                        marginRight: "10%",
+                        marginTop: "2%"
+                    }} 
+                    alt="BullsCryptoBox" 
+                />
+            </div>
+            <div
+                id="santaBullDiv"
+                style={{
+                    marginTop: 0.9 * -SantaBullDivHeight
+                }}>
+                <img 
+                    src={santaBull} 
+                    style={{
+                        width: width * 0.22,
+                        marginLeft: "55%",
+
+                    }} 
+                    alt="BullsCryptoSantaBull" 
+                />
+            </div>
+        </div>
+    )
+}
+
+export default SecondBlock

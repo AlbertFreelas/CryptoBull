a88fd615f6f1ae8c1067a4f263935cff3c420387
AlbertSamuelMelo <asm.asm.albert@icloud.com>
2021-12-29 20:01:25 -0400

    added play and whitepapper
    Signed-off-by: AlbertSamuelMelo <asm.asm.albert@icloud.com>

--- src/components/pages/FristBlock/FirstBlock.js
+++ src/components/pages/FristBlock/FirstBlock.js
@@ -3,6 +3,10 @@ import firstBackground from './../../../first-block/FUNDO-TELA01@2x.png'
 import ledBar from './../../../first-block/FUNDO-TELA02@2x.png'
 import ledString from './../../../first-block/FUNDO-TELA03@2x.png'
 import arm from './../../../first-block/FUNDO-TELA04@2x.png'
+
+import play from './../../../first-block/PLAY-BOTÃO@2x.png'
+import withepapper from './../../../first-block/WHITEPAPER-BOTÃO@2x.png'
+
 import './FirstBlock.css';
 
 import { useState, useEffect } from 'react';
@@ -11,22 +15,16 @@ function FirstBlock() {
 
     const [width, setWidth] = useState(window.innerWidth)
     const [ledDivHeight, setLedDivHeight] = useState(0)
-    const [armDivHeight, setArmDivHeight] = useState(0)
 
     useEffect(() => {
         window.addEventListener('load', () => {
             var updatedHeight = document.getElementById("ledDiv").clientHeight
             setLedDivHeight(updatedHeight)
-
-            var updatedArmHeight = document.getElementById("background").clientHeight
-            setArmDivHeight(updatedHeight)
         });
         window.addEventListener('resize', () => {
             setWidth(window.innerWidth)
             var updatedHeight = document.getElementById("ledDiv").clientHeight
             setLedDivHeight(updatedHeight)
-            var updatedArmHeight = document.getElementById("background").clientHeight
-            setArmDivHeight(updatedHeight)
         });
       });
 
@@ -36,6 +34,35 @@ function FirstBlock() {
             flexDirection: "column",
             flex: "auto",
         }}>
+            <div 
+                style = {{
+                    position:"absolute",
+                }}>
+                    <div>
+                        <img
+                            src={withepapper} 
+                            style={{
+                                width: 0.2 * width,
+                                marginTop: "8%",
+                                marginLeft: "380%",
+                                pointerEvents: "none"
+                            }} 
+                            alt="BullsCryptoBackground" 
+                        />
+                    </div>
+                    <div>
+                        <img
+                            src={play} 
+                            style={{
+                                width: 0.2 * width,
+                                marginTop: "180%",
+                                marginLeft: "200%",
+                                pointerEvents: "none"
+                            }} 
+                            alt="BullsCryptoBackground" 
+                        />
+                    </div>
+                </div>
             <div
                 id='background'
             >
